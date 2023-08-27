a733e73a5e66cc21789c5ede92faaefeab56f6e5
Mark Miller <mark.o.miller@gmail.com>
2023-08-27 13:19:08 -0600

    display headers

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,22 @@
 import type { PageServerLoad } from './$types'
 
-export const load = (async ({ url, cookies }) => {
+export const load = (async ({ url, cookies, request }) => {
 
+    const mapHeaders = () => {
+
+        const h = [];
+        for (const header of request.headers.entries()) {
+            h.push(header);
+        }
+        return h;
+    }
+    const c = cookies.getAll()
+
+    console.log(c);
     return {
         host: url.host,
-        renderedTS: new Date().getTime()
+        renderedTS: new Date().getTime(),
+        headers: mapHeaders(),
+        cookies: c
     }
 }) satisfies PageServerLoad
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,9 @@
 
 <h1>portal.three2s.com</h1>
 <p>[served from: {data.host}]</p>
+
+<h2>Request Cookies</h2>
+<pre>{JSON.stringify(data.cookies, null, 2)}</pre>
+
+<h2>Request Headers</h2>
+<pre>{JSON.stringify(data.headers, null, 2)}</pre>
