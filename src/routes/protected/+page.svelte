ea35432fae95b1cdc6d2b5adbdf17a8fb4320409
Mark Miller <mark.o.miller@gmail.com>
2023-08-28 21:06:27 -0600

    show session

--- src/routes/protected/+page.svelte
+++ src/routes/protected/+page.svelte
@@ -4,6 +4,7 @@
 
 {#if $page.data.session}
 	<h1>Protected page</h1>
+	<pre style="color:blue">Session: {JSON.stringify($page.data.session, null, 2)}</pre>
 	<p>This is a protected content. You can access this content because you are signed in.</p>
 	<p>Session expiry: {$page.data.session?.expires}</p>
 {:else}
